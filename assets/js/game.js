5e0a4ad27eae400c5d31336ad6c169ad8ea50622
Paul Barcenas <paulbarcenas@yahoo.com>
2021-08-20 00:06:23 -0500

    updated shop to Handle Integer Inputs

--- assets/js/game.js
+++ assets/js/game.js
@@ -139,20 +139,19 @@ var endGame = function() {
 var shop = function() {
     // ask player what they'd like to do
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', ' UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 REFILL, 2 UPGRADE, or 3 for LEAVE."
     );
     // use switch to carry out action
+    shopOptionPrompt = parseInt(shopOptionPrompt);
     switch (shopOptionPrompt) {
-        case "refill":
-        case "REFILL":
+        
+        case 1:
             playerInfo.refillHealth();           
             break;
-        case "upgrade":
-        case "UPGRADE":
+        case 2:
             playerInfo.upgradeAttack();
             break;
-        case "leave":
-        case "LEAVE":
+        case 3:
             window.alert("Leaving the store.");
 
             // do nothing, so function will end
